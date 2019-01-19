9be5d6aa457ebac2d004accfe63fa8e3f12f03b5
Kevin Jump <kevin@thejumps.co.uk>
2019-01-19 23:25:07 +0000

    Additional ContentType Serialization bits

--- uSync8.Core/Serialization/Serializers/ContentTypeSerializer.cs
+++ uSync8.Core/Serialization/Serializers/ContentTypeSerializer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Xml.Linq;
 using Umbraco.Core.Models;
 using Umbraco.Core.Models.Entities;
@@ -27,13 +28,50 @@ namespace uSync8.Core.Serialization.Serializers
         {
             var info = SerializeInfo(item);
 
-            // templates
-            info.Add(SerailizeTemplates(item));
+            var parent = item.ContentTypeComposition.FirstOrDefault(x => x.Id == item.ParentId);
+            if (parent != null)
+            {
+                info.Add(new XElement("Master", parent.Alias,
+                            new XAttribute("Key", parent.Key)));
+            }
+            else if (item.Level != 1)
+            {
+                // in a folder. 
+                var folders = contentTypeService.GetContainers(item)
+                    .OrderBy(x => x.Level)
+                    .Select(x => HttpUtility.UrlEncode(x.Name))
+                    .ToList();
+
+                if (folders.Any())
+                {
+                    string path = string.Join("/", folders);
+                    info.Add(new XElement("Folder", path));
+                }
+            }
 
             // compositions ? 
             // (might be in the ContentTypeCore, because you can also do this
             //  with media?)
-   
+            var compNode = new XElement("Compositions");
+            var compositions = item.ContentTypeComposition;
+            foreach (var composition in compositions.OrderBy(x => x.Key))
+            {
+                compNode.Add(new XElement("Composition", composition.Alias,
+                    new XAttribute("Key", composition.Key)));
+            }
+            info.Add(compNode);
+
+            // templates
+            var templateAlias =
+                (item.DefaultTemplate != null && item.DefaultTemplate.Id != 0)
+                ? item.DefaultTemplate.Alias
+                : "";
+
+            info.Add(new XElement("DefaultTemplate", templateAlias));
+
+            var templates = SerailizeTemplates(item);
+            if (templates != null)
+                info.Add(templates);
 
             var node = new XElement(ItemType,
                 info,
