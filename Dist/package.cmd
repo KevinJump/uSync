8ddf412abacc32a29a8e1dfe1e6da72b74612f44
Kevin Jump <kevin@thejumps.co.uk>
2019-04-26 17:13:49 +0100

    update package build (dependency variables)

--- Dist/package.cmd
+++ Dist/package.cmd
@@ -1,7 +1,7 @@
 
 @Echo Packaging
-nuget pack ..\uSync8.Core\uSync.Core.nuspec -build -version %1
-nuget pack ..\uSync8.BackOffice\uSync.nuspec -build -version %1
-nuget pack ..\uSync8.ContentEdition\uSync.ContentEdition.nuspec -build -version %1
+nuget pack ..\uSync8.Core\uSync.Core.nuspec -build  -OutputDirectory %1 -version %1  -properties depends=%1
+nuget pack ..\uSync8.BackOffice\uSync.nuspec -build  -OutputDirectory %1 -version %1 -properties depends=%1
+nuget pack ..\uSync8.ContentEdition\uSync.ContentEdition.nuspec  -OutputDirectory %1 -build -version %1  -properties depends=%1
 
-XCOPY *.%1.nupkg c:\source\localgit /y
+XCOPY %1\*.nupkg c:\source\localgit /y
