81452c3e8f193663e263cdc71c5e2a2f2efe48ee
Kevin Jump <kevin@thejumps.co.uk>
2019-05-09 17:36:09 +0100

    v8.0 build process.

--- Dist/package.cmd
+++ Dist/package.cmd
@@ -1,8 +1,18 @@
+@echo off 
 
 @Echo Packaging
 nuget pack ..\uSync8.Core\uSync.Core.nuspec -build  -OutputDirectory %1 -version %1  -properties depends=%1
 nuget pack ..\uSync8.BackOffice\uSync.nuspec -build  -OutputDirectory %1 -version %1 -properties depends=%1
 nuget pack ..\uSync8.BackOffice\uSync.BackOffice.Core.nuspec -build  -OutputDirectory %1 -version %1 -properties depends=%1
 nuget pack ..\uSync8.ContentEdition\uSync.ContentEdition.nuspec  -OutputDirectory %1 -build -version %1  -properties depends=%1
+
+
+CALL UmbPackage package.xml %1
+CALL "c:\Program Files\7-Zip\7z.exe" a .\%1\uSync_%1.zip .\%1\uSync_%1\*.* 
+CALL RD .\%1\uSync_%1\ /s /q
+
+CALL UmbPackage package.ContentEdition.xml %1
+CALL "c:\Program Files\7-Zip\7z.exe" a .\%1\uSync.ContentEdition_%1.zip .\%1\uSync.ContentEdition_%1\*.* 
+CALL RD .\%1\uSync.ContentEdition_%1\ /s /q
 
 XCOPY %1\*.nupkg c:\source\localgit /y
