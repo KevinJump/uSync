db7709c7baf2f17fbfe32c1f3a5c1fb08ebcf9aa
Kevin Jump <kevin@thejumps.co.uk>
2019-06-26 14:00:14 +0100

    Add Component views for common UI

---
+++ uSync8.BackOffice/App_Plugins/uSync8/components/usyncProgressViewComponent.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    'use strict';
+
+    var uSyncProgressViewComponent = {
+        templateUrl: Umbraco.Sys.ServerVariables.application.applicationPath + 'App_Plugins/uSync8/Components/uSyncProgressView.html',
+        bindings: {
+            status: '<',
+            update: '<',
+            hideLabels: '<'
+        },
+        controllerAs: 'vm',
+        controller: uSyncProgressViewController
+    };
+
+    function uSyncProgressViewController() {
+        var vm = this;
+
+        vm.calcPercentage = calcPercentage;
+
+        function calcPercentage(status) {
+            if (status !== undefined) {
+                return (100 * status.Count) / status.Total;
+            }
+            return 1;
+        }
+    }
+
+    angular.module('umbraco')
+        .component('usyncProgressView', uSyncProgressViewComponent);
+})();
