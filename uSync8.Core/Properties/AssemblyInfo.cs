e3a952e06ef8d64b3cf99bb285c8a4e283056f98
Kevin Jump <kevin@thejumps.co.uk>
2019-07-22 09:06:24 +0100

    8.1.1 - Assembly Build Numbers.

--- uSync8.Core/Properties/AssemblyInfo.cs
+++ uSync8.Core/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.0.1.611")]
-[assembly: AssemblyFileVersion("8.0.1.611")]
+[assembly: AssemblyVersion("8.1.1.722")]
+[assembly: AssemblyFileVersion("8.1.1.722")]
