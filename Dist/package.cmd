00b04a5a7328cfcac855c9ba70583998dbaba481
Kevin Jump <kevin@thejumps.co.uk>
2019-08-10 20:15:56 +0100

    Update nuspec folder refrences

--- Dist/package.cmd
+++ Dist/package.cmd
@@ -1,10 +1,10 @@
 @echo off 
 
 @Echo Packaging
-nuget pack ..\uSync8.Core\uSync.Core.nuspec -build  -OutputDirectory %1 -version %1  -properties depends=%1
-nuget pack ..\uSync8.BackOffice\uSync.nuspec -build  -OutputDirectory %1 -version %1 -properties depends=%1
-nuget pack ..\uSync8.BackOffice\uSync.BackOffice.Core.nuspec -build  -OutputDirectory %1 -version %1 -properties depends=%1
-nuget pack ..\uSync8.ContentEdition\uSync.ContentEdition.nuspec  -OutputDirectory %1 -build -version %1  -properties depends=%1
+nuget pack ..\uSync8.Core\uSync.Core.nuspec -build  -OutputDirectory %1 -version %1 -properties "depends=%1;Configuration=release"
+nuget pack ..\uSync8.BackOffice\uSync.nuspec -build  -OutputDirectory %1 -version %1 -properties "depends=%1;Configuration=release"
+nuget pack ..\uSync8.BackOffice\uSync.BackOffice.Core.nuspec -build  -OutputDirectory %1 -version %1 -properties "depends=%1;Configuration=release"
+nuget pack ..\uSync8.ContentEdition\uSync.ContentEdition.nuspec  -OutputDirectory %1 -build -version %1 -properties "depends=%1;Configuration=release"
 
 call CreatePackages %1
 
