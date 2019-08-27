2bf237733242ca1da71f99746a3e76fe384a479a
Kevin Jump <kevin@jumoo.co.uk>
2019-08-27 16:10:53 +0100

    beta 2 prep

--- uSync8.ContentEdition/Properties/AssemblyInfo.cs
+++ uSync8.ContentEdition/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.2.0.806")]
-[assembly: AssemblyFileVersion("8.2.0.806")]
+[assembly: AssemblyVersion("8.2.0.827")]
+[assembly: AssemblyFileVersion("8.2.0.827")]
