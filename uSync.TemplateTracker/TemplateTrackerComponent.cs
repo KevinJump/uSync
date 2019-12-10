82092bc46cef933ff19f3cf70db10aeb67db9c46
Kevin Jump <kevin@jumoo.co.uk>
2019-12-10 23:18:26 +0000

    tracker - experiment (watcher doesn't work at the moment)

---
+++ uSync.TemplateTracker/TemplateTrackerComponent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace uSync.TemplateTracker
+{
+    public class TemplateTrackerCompose 
+        : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.RegisterUnique<TemplateTracker>();
+            composition.Components().Append<TemplateTrackerComponent>();
+        }
+    }
+
+    public class TemplateTrackerComponent : IComponent
+    {
+        private readonly TemplateTracker tracker;
+
+        public TemplateTrackerComponent(TemplateTracker tracker)
+        {
+            this.tracker = tracker;
+        }
+
+        public void Initialize()
+        {
+            tracker.TrackChanges();
+            // tracker.WatchViewFolder();
+        }
+
+        public void Terminate()
+        {
+            // do nothing.
+        }
+    }
+}
