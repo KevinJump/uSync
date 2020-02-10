91f97c97734df72f1215379eb2603bd4966a8c48
Kevin Jump <kevin@jumoo.co.uk>
2020-02-10 12:12:37 +0000

    Fix: Repeatable String missing carrage return on import.

---
+++ uSync8.ContentEdition/Mapping/Mappers/RepeatableValueMapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Umbraco.Core.Services;
+
+namespace uSync8.ContentEdition.Mapping.Mappers
+{
+    /// <summary>
+    ///  restore the /r/n to the string when the xml strips it out :( 
+    /// </summary>
+    public class RepeatableValueMapper : SyncValueMapperBase, ISyncMapper
+    {
+        public RepeatableValueMapper(IEntityService entityService)
+            : base(entityService)
+        { }
+
+        public override string Name => "Repeatable Text Mapper";
+
+        public override string[] Editors => new string[] {
+            "Umbraco.MultipleTextstring"
+        };
+
+        public override string GetImportValue(string value, string editorAlias)
+        {
+            if (!value.Contains('\r'))
+            {
+                return value.Replace("\n", "\r\n");
+            }
+
+            return value;
+        }
+    }
+}
