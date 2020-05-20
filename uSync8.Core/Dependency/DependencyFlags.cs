d32db787786687a4d4e41d32f8ceaf7720cfea70
Kevin Jump <kevin@jumoo.co.uk>
2020-05-20 14:47:41 +0100

    Extend dependency flags to include item config.

--- uSync8.Core/Dependency/DependencyFlags.cs
+++ uSync8.Core/Dependency/DependencyFlags.cs
@@ -6,12 +6,45 @@ namespace uSync8.Core.Dependency
     public enum DependencyFlags
     {
         None = 0,
+
+        /// <summary>
+        ///  Include child items of this item
+        /// </summary>
         IncludeChildren = 2,
+
+        /// <summary>
+        ///  Include parent items of this item
+        /// </summary>
         IncludeAncestors = 4,
+
+        /// <summary>
+        ///  include any items required to create this item (e.g doctypes/datatypes)
+        /// </summary>
         IncludeDependencies = 8,
+
+        /// <summary>
+        ///  include the view templates associated with an item
+        /// </summary>
         IncludeViews = 16,
+
+        /// <summary>
+        ///  include any media used within a content item 
+        /// </summary>
         IncludeMedia = 32,
+
+        /// <summary>
+        ///  include any items that are linked to (via pickers) of this item (can result in big dependency tree)
+        /// </summary>
         IncludeLinked = 64,
-        IncludeMediaFiles = 128
+
+        /// <summary>
+        ///  include the actual media files (e.g the images) 
+        /// </summary>
+        IncludeMediaFiles = 128,
+
+        /// <summary>
+        ///  Include configuration related to an item (e.g domains/public access settings etc)
+        /// </summary>
+        IncludeConfig = 256
     }
 }
