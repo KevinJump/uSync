cb3b44de8ea74957b773f7b20dbea332385433df
Kevin Jump <kevin@jumoo.co.uk>
2020-06-29 11:26:50 +0100

    Comment up the constants classes.

--- uSync8.BackOffice/BackOfficeConstants.cs
+++ uSync8.BackOffice/BackOfficeConstants.cs
@@ -2,6 +2,23 @@
 {
     public static partial class uSyncBackOfficeConstants
     {
+        /// <summary>
+        ///  priorities determain the order things get imported in.
+        /// </summary>
+        /// <remarks>
+        ///  In general: 
+        ///    - datatypes have to be before everything
+        ///    - contentTypes/mediatypes/etc require datatypes and templates
+        ///    - media/content need everthing setup
+        ///    - domains and relations are at the end. 
+        ///    
+        ///  custom things should go for numbers passed USYNC_RESERVED_UPPER
+        ///  to ensure they happen at the end. 
+        ///     
+        ///  Where there are circular dependencies (e.g doctypes that refrence
+        ///  content structures). We have post processing, in that things will
+        ///  be called again at the end of the process (after everything else) 
+        /// </remarks>
         public static class Priorites
         {
             public const int USYNC_RESERVED_LOWER = 1000;
@@ -28,6 +45,20 @@
 
             public const int DataTypeMappings = USYNC_RESERVED_LOWER + 250;
         }
+
+        /// <summary>
+        ///  Sync groups for handlers when only doing 'part' sync
+        /// </summary>
+        /// <remarks>
+        ///  If the handler doesn't speficiy a group its treated as being part 
+        ///  of the 'settings' group.
+        /// 
+        ///  Handlers are not limited to these groups, but if you have a custom
+        ///  one the UI will look for language keys (in the xml files)
+        ///  
+        ///    usync_import-[name] and usync_report-[name]
+        ///    
+        /// </remarks>
 
         public static class Groups
         {
