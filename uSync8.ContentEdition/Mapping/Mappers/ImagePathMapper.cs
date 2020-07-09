71413b3966c06c400d233b3c02183ea025e25a3e
Kevin Jump <kevin@jumoo.co.uk>
2020-07-09 15:41:12 +0100

    #128 make media path generic if using a container name.

--- uSync8.ContentEdition/Mapping/Mappers/ImagePathMapper.cs
+++ uSync8.ContentEdition/Mapping/Mappers/ImagePathMapper.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
+
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 using Umbraco.Core;
 using Umbraco.Core.IO;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Models;
 using Umbraco.Core.Services;
+
+using uSync8.BackOffice.Configuration;
 using uSync8.Core.Dependency;
 
 namespace uSync8.ContentEdition.Mapping.Mappers
@@ -25,10 +32,15 @@ namespace uSync8.ContentEdition.Mapping.Mappers
     public class ImagePathMapper : SyncValueMapperBase, ISyncMapper
     {
         private readonly string siteRoot;
+        private readonly string mediaFolder; 
 
-        public ImagePathMapper(IEntityService entityService) : base(entityService)
+        public ImagePathMapper(IEntityService entityService, 
+            IProfilingLogger logger, uSyncConfig config) : base(entityService)
         {
             siteRoot = SystemDirectories.Root;
+            mediaFolder = GetMediaFolderSetting(config);
+
+            logger.Debug<ImagePathMapper>("Media Folders: [{media}]", mediaFolder);
         }
 
         public override string Name => "ImageCropper Mapper";
@@ -69,19 +81,77 @@ namespace uSync8.ContentEdition.Mapping.Mappers
 
         private string StripSitePath(string filepath)
         {
+            var path = filepath;
             if (siteRoot.Length > 0 && !string.IsNullOrWhiteSpace(filepath) && filepath.InvariantStartsWith(siteRoot))
-                return filepath.Substring(siteRoot.Length);
+                path = filepath.Substring(siteRoot.Length);
 
-            return filepath;
+            return ReplacePath(path, mediaFolder, "/media");
         }
 
         private string PrePendSitePath(string filepath)
         {
+            var path = filepath;
             if (siteRoot.Length > 0 && !string.IsNullOrEmpty(filepath))
-                return $"{siteRoot}{filepath}";
+                path = $"{siteRoot}{filepath}";
+
+            return ReplacePath(path, "/media", mediaFolder);
+        }
+
+
+        /// <summary>
+        ///  makes a specific media path generic. 
+        /// </summary>
+        /// <remarks>
+        ///  sometimes paths may be defined by umbraco settings, (especially blob settings)
+        ///  that mean they are not stored as /media 
+        ///  
+        ///  for the sake of generic importing we want the folder stored to be /media. 
+        ///  so we re-write the setting on import and export 
+        ///  
+        ///  assumes you have a app setting in the web.config 
+        ///  
+        ///     <add key="uSync.mediaFolder">/somefolder</add>
+        ///
+        /// </remarks>
+        /// <returns></returns>
+        private string ReplacePath(string filepath, string currentPath, string targetPath)
+        {
+            if (!string.IsNullOrWhiteSpace(targetPath)
+                && !string.IsNullOrWhiteSpace(currentPath))
+            {
+                return Regex.Replace(filepath, $"^{currentPath}", targetPath, RegexOptions.IgnoreCase);
+            }
 
             return filepath;
         }
+
+        /// <summary>
+        ///  Get the media rewrite folder 
+        /// </summary>
+        /// <remarks>
+        ///     looks in appSettings for uSync:mediaFolder 
+        ///     
+        ///  <add key="uSync.mediaFolder" value="/something" />
+        /// 
+        ///  or in uSync8.config for media setting 
+        ///  
+        ///  <backoffice>
+        ///     <media>
+        ///         <folder>/somefolder</folder>
+        ///     </media>
+        ///  </backoffice>
+        /// </remarks>
+        private string GetMediaFolderSetting(uSyncConfig config)
+        {
+            // look in the web.config 
+            var folder = ConfigurationManager.AppSettings["uSync.mediaFolder"];
+            if (!string.IsNullOrWhiteSpace(folder))
+                return folder;
+
+            // look in the uSync8.config 
+            return config.GetExtensionSetting("media", "folder", string.Empty);
+        }
+
 
         public override string GetImportValue(string value, string editorAlias)
         {
