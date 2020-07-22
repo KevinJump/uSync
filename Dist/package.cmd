213bd44653e814a746ebac536e1f3c6bea747254
Kevin Jump <kevin@jumoo.co.uk>
2020-07-22 14:11:46 +0100

    set package stuff to release.

--- Dist/package.cmd
+++ Dist/package.cmd
@@ -1,7 +1,7 @@
 @echo off 
 
-REM SET config=release
-SET config=debug
+SET config=release
+REM SET config=debug
 
 @Echo Packaging for %config%
 nuget pack ..\uSync8.Core\uSync.Core.nuspec -build  -OutputDirectory %1 -version %1 -properties "depends=%1;Configuration=%config%"
