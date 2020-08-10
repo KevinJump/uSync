0d351d0ce7cb67e21a2dde24064af8bed0ae1cf2
Kevin Jump <kevin@jumoo.co.uk>
2020-08-10 12:31:45 +0100

    update package commands to work with actions.

--- Dist/package.cmd
+++ Dist/package.cmd
@@ -3,20 +3,24 @@
 SET config=release
 REM SET config=debug
 
+CALL MsBuild .\uSync8.sln -t:Rebuild -p:Configuration=%config% -clp:Verbosity=minimal;Summary
+
 @Echo Packaging for %config%
-nuget pack ..\uSync8.Core\uSync.Core.nuspec -build  -OutputDirectory %1 -version %1 -properties "depends=%1;Configuration=%config%"
-nuget pack ..\uSync8.BackOffice\uSync.nuspec -build  -OutputDirectory %1 -version %1 -properties "depends=%1;Configuration=%config%"
-nuget pack ..\uSync8.BackOffice\uSync.BackOffice.Core.nuspec -build  -OutputDirectory %1 -version %1 -properties "depends=%1;Configuration=%config%"
-nuget pack ..\uSync8.Community.Contrib\uSync.Community.Contrib.nuspec  -OutputDirectory %1 -build -version %1 -properties "depends=%1;Configuration=%config%"
-nuget pack ..\uSync8.ContentEdition\uSync.ContentEdition.nuspec  -OutputDirectory %1 -build -version %1 -properties "depends=%1;Configuration=%config%"
-nuget pack ..\uSync8.ContentEdition\uSync.ContentEdition.Core.nuspec  -OutputDirectory %1 -build -version %1 -properties "depends=%1;Configuration=%config%"
+nuget pack .\uSync8.Core\uSync.Core.nuspec -build  -OutputDirectory .\dist\%1 -version %1 -properties "depends=%1;Configuration=%config%"
+nuget pack .\uSync8.BackOffice\uSync.nuspec -build  -OutputDirectory .\dist\%1 -version %1 -properties "depends=%1;Configuration=%config%"
+nuget pack .\uSync8.BackOffice\uSync.BackOffice.Core.nuspec -build  -OutputDirectory .\dist\%1 -version %1 -properties "depends=%1;Configuration=%config%"
+nuget pack .\uSync8.Community.Contrib\uSync.Community.Contrib.nuspec  -OutputDirectory .\dist\%1 -build -version %1 -properties "depends=%1;Configuration=%config%"
+nuget pack .\uSync8.ContentEdition\uSync.ContentEdition.nuspec  -OutputDirectory .\dist\%1 -build -version %1 -properties "depends=%1;Configuration=%config%"
+nuget pack .\uSync8.ContentEdition\uSync.ContentEdition.Core.nuspec  -OutputDirectory .\dist\%1 -build -version %1 -properties "depends=%1;Configuration=%config%"
 
-nuget pack ..\uSync8.Community.DataTypeSerializers\uSync.Community.DataTypeSerializers.nuspec  -OutputDirectory %1 -build -version %1 -properties "depends=%1;Configuration=%config%"
+nuget pack .\uSync8.Community.DataTypeSerializers\uSync.Community.DataTypeSerializers.nuspec  -OutputDirectory .\dist\%1 -build -version %1 -properties "depends=%1;Configuration=%config%"
 
-nuget pack ..\uSync.Console\uSync.Console.nuspec  -OutputDirectory %1 -build -version %1 -properties "depends=%1;Configuration=%config%"
+nuget pack .\uSync.Console\uSync.Console.nuspec  -OutputDirectory .\dist\%1 -build -version %1 -properties "depends=%1;Configuration=%config%"
 
-call CreatePackages %1
+call .\dist\CreatePackages %1
 
-XCOPY %1\*.nupkg c:\source\localgit /y
+@ECHO Copying to LocalGit Folder
+XCOPY .\dist\%1\*.nupkg c:\source\localgit /y
 
 ECHO Packaging Complete (%config% build)
+
