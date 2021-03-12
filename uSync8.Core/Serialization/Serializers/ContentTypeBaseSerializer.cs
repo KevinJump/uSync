f8b42f48e31399041a63979d353a5e8ade1565d6
Kevin Jump <kevin@jumoo.co.uk>
2021-03-12 12:12:24 +0000

    Fix: put the on LabelOnTop property back right this time #209

--- uSync8.Core/Serialization/Serializers/ContentTypeBaseSerializer.cs
+++ uSync8.Core/Serialization/Serializers/ContentTypeBaseSerializer.cs
@@ -116,7 +116,7 @@ namespace uSync8.Core.Serialization.Serializers
                 // added in 8.10
                 if (UmbracoVersion.LocalVersion.Major > 8 || UmbracoVersion.LocalVersion.Minor >= 10)
                 {
-                    SerializeNewProperty<bool>(propNode, property, "labelOnTop");
+                    SerializeNewProperty<bool>(propNode, property, "LabelOnTop");
                 }
 
                 node.Add(propNode);
@@ -443,7 +443,7 @@ namespace uSync8.Core.Serialization.Serializers
 
                 if (UmbracoVersion.LocalVersion.Major > 8 || UmbracoVersion.LocalVersion.Minor >= 10)
                 {
-                    changes.AddNotNull(DeserializeNewProperty<bool>(property, propertyNode, "labelOnTop"));
+                    changes.AddNotNull(DeserializeNewProperty<bool>(property, propertyNode, "LabelOnTop"));
                 }
 
                 changes.AddRange(DeserializeExtraProperties(item, property, propertyNode));
