ca25f2eec51127b793d7b372380bcc6318b88141
Kevin Jump <kevin@jumoo.co.uk>
2021-04-14 15:12:19 +0100

    Remove data and console packages from the build script

--- Dist/package.cmd
+++ Dist/package.cmd
@@ -13,9 +13,9 @@ nuget pack .\uSync8.Community.Contrib\uSync.Community.Contrib.nuspec  -OutputDir
 nuget pack .\uSync8.ContentEdition\uSync.ContentEdition.nuspec  -OutputDirectory .\dist\%1 -build -version %1 -properties "depends=%1;Configuration=%config%"
 nuget pack .\uSync8.ContentEdition\uSync.ContentEdition.Core.nuspec  -OutputDirectory .\dist\%1 -build -version %1 -properties "depends=%1;Configuration=%config%"
 
-nuget pack .\uSync8.Community.DataTypeSerializers\uSync.Community.DataTypeSerializers.nuspec  -OutputDirectory .\dist\%1 -build -version %1 -properties "depends=%1;Configuration=%config%"
+REM nuget pack .\uSync8.Community.DataTypeSerializers\uSync.Community.DataTypeSerializers.nuspec  -OutputDirectory .\dist\%1 -build -version %1 -properties "depends=%1;Configuration=%config%"
+REM nuget pack .\uSync.Console\uSync.Console.nuspec  -OutputDirectory .\dist\%1 -build -version %1 -properties "depends=%1;Configuration=%config%"
 
-nuget pack .\uSync.Console\uSync.Console.nuspec  -OutputDirectory .\dist\%1 -build -version %1 -properties "depends=%1;Configuration=%config%"
 nuget pack .\uSync8.HistoryView\uSync.History.nuspec  -OutputDirectory .\dist\%1 -build -version %1 -properties "depends=%1;Configuration=%config%"
 
 call .\dist\CreatePackages %1
