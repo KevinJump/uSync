575810befadbe2efbfbc7c88c517b6ab3a7fd8cc
Kevin Jump <kevin@jumoo.co.uk>
2021-04-24 08:26:53 +0100

    update to include source link, and determanistic build in script.

--- dist/build-package.ps1
+++ dist/build-package.ps1
@@ -1,4 +1,4 @@
 param ($suffix, $env='release')
 
-dotnet pack ..\uSync.Core\uSync.Core.csproj -c $env -o .\nightly --version-suffix $suffix
-dotnet pack ..\uSync.BackOffice\uSync.BackOffice.csproj -c $env -o .\nightly --version-suffix $suffix
+dotnet pack ..\uSync.Core\uSync.Core.csproj -c $env -o .\nightly --version-suffix $suffix /p:ContinuousIntegrationBuild=true 
+dotnet pack ..\uSync.BackOffice\uSync.BackOffice.csproj -c $env -o .\nightly --version-suffix $suffix /p:ContinuousIntegrationBuild=true 
