4b8459f5f8ca5cbfa0844294b328ee9cf50c8bda
Kevin Jump <kevin@thejumps.co.uk>
2021-04-28 16:59:33 +0100

    build folder output change

--- dist/build-package.ps1
+++ dist/build-package.ps1
@@ -1,5 +1,5 @@
 param ($suffix, $env='release')
 
-dotnet pack ..\uSync.Core\uSync.Core.csproj -c $env -o .\nightly --version-suffix $suffix /p:ContinuousIntegrationBuild=true 
-dotnet pack ..\uSync.Community.Contrib\uSync.Community.Contrib.csproj -c $env -o .\nightly --version-suffix $suffix /p:ContinuousIntegrationBuild=true 
-dotnet pack ..\uSync.BackOffice\uSync.BackOffice.csproj -c $env -o .\nightly --version-suffix $suffix /p:ContinuousIntegrationBuild=true 
+dotnet pack ..\uSync.Core\uSync.Core.csproj -c $env -o .\$suffix --version-suffix $suffix /p:ContinuousIntegrationBuild=true 
+dotnet pack ..\uSync.Community.Contrib\uSync.Community.Contrib.csproj -c $env -o .\$suffix --version-suffix $suffix /p:ContinuousIntegrationBuild=true 
+dotnet pack ..\uSync.BackOffice\uSync.BackOffice.csproj -c $env -o .\$suffix --version-suffix $suffix /p:ContinuousIntegrationBuild=true 
