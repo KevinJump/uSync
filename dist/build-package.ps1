aa7a39452b80921093776d0ddd2b164e46d4f3ad
Kevin Jump <kevin@thejumps.co.uk>
2021-04-29 10:17:23 +0100

    push to nightly option.

--- dist/build-package.ps1
+++ dist/build-package.ps1
@@ -1,5 +1,9 @@
-param ($suffix, $env='release')
+param ($suffix, $env='release', [switch]$push=$false)
 
 dotnet pack ..\uSync.Core\uSync.Core.csproj -c $env -o .\$suffix --version-suffix $suffix /p:ContinuousIntegrationBuild=true 
 dotnet pack ..\uSync.Community.Contrib\uSync.Community.Contrib.csproj -c $env -o .\$suffix --version-suffix $suffix /p:ContinuousIntegrationBuild=true 
 dotnet pack ..\uSync.BackOffice\uSync.BackOffice.csproj -c $env -o .\$suffix --version-suffix $suffix /p:ContinuousIntegrationBuild=true 
+
+if ($push) {
+    nuget push .\$suffix\*.nupkg -ApiKey AzureDevOps -src https://pkgs.dev.azure.com/jumoo/Public/_packaging/nightly/nuget/v3/index.json
+}
