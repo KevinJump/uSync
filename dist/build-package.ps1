376139b99668f2895b75a1dfbeb956533a90dea6
Kevin Jump <kevin@thejumps.co.uk>
2021-07-02 11:16:36 +0100

    update build script

--- dist/build-package.ps1
+++ dist/build-package.ps1
@@ -1,12 +1,15 @@
-param ($suffix, $env='release', [switch]$push=$false)
+param ($version = '9.0.0', $suffix, $env='release', [switch]$push=$false)
 
-dotnet pack ..\uSync.Core\uSync.Core.csproj -c $env -o .\$suffix --version-suffix $suffix /p:ContinuousIntegrationBuild=true 
-dotnet pack ..\uSync.Community.Contrib\uSync.Community.Contrib.csproj -c $env -o .\$suffix --version-suffix $suffix /p:ContinuousIntegrationBuild=true 
-dotnet pack ..\uSync.BackOffice\uSync.BackOffice.csproj -c $env -o .\$suffix --version-suffix $suffix /p:ContinuousIntegrationBuild=true 
+$fullVersion = -join($version, '-', $suffix)
+$outFolder = ".\$fullVersion"
 
-.\nuget pack "..\uSync\uSync.nuspec" -version 9.0.0-$suffix -OutputDirectory .\$suffix
-.\nuget pack "..\uSync.BackOffice.Assets\uSync.BackOffice.StaticAssets.nuspec" -version 9.0.0-$suffix -OutputDirectory .\$suffix
+dotnet pack ..\uSync.Core\uSync.Core.csproj -c $env -o $outFolder --version-suffix $suffix /p:ContinuousIntegrationBuild=true,version=$version 
+dotnet pack ..\uSync.Community.Contrib\uSync.Community.Contrib.csproj -c $env -o $outFolder --version-suffix $suffix /p:ContinuousIntegrationBuild=true,version=$version  
+dotnet pack ..\uSync.BackOffice\uSync.BackOffice.csproj -c $env -o $outFolder --version-suffix $suffix /p:ContinuousIntegrationBuild=true,version=$version 
+
+.\nuget pack "..\uSync\uSync.nuspec" -version 9.0.0-$suffix -OutputDirectory $outFolder
+.\nuget pack "..\uSync.BackOffice.Assets\uSync.BackOffice.StaticAssets.nuspec" -version $fullVersion -OutputDirectory $outFolder
 
 if ($push) {
-    .\nuget.exe push ".\$suffix\*.nupkg" -ApiKey AzureDevOps -src https://pkgs.dev.azure.com/jumoo/Public/_packaging/nightly/nuget/v3/index.json
+    .\nuget.exe push "$outFolder\*.nupkg" -ApiKey AzureDevOps -src https://pkgs.dev.azure.com/jumoo/Public/_packaging/nightly/nuget/v3/index.json
 }
