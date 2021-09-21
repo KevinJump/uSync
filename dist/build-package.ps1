c241410ab109799a841bb1a1123f37ecb83f7e79
Kevin Jump <kevin@thejumps.co.uk>
2021-09-21 14:34:17 +0100

    update v9 - build powershell.

--- dist/build-package.ps1
+++ dist/build-package.ps1
@@ -1,21 +1,65 @@
-param ($version = '9.0.0', $suffix, $env='release', [switch]$push=$false)
+<#
+SYNOPSIS
+    Buils the optionally pushes the uSync packages.
+#>
+param (
 
-$fullVersion = -join($version, '-', $suffix)
-$outFolder = ".\$fullVersion"
+    [Parameter(Mandatory)]
+    [string]
+    [Alias("v")]  $version, #version to build
 
-dotnet pack ..\uSync.Core\uSync.Core.csproj -c $env -o $outFolder /p:ContinuousIntegrationBuild=true,version=$fullVersion 
-dotnet pack ..\uSync.Community.Contrib\uSync.Community.Contrib.csproj -c $env -o $outFolder /p:ContinuousIntegrationBuild=true,version=$fullversion  
-dotnet pack ..\uSync.Community.DataTypeSerializers\uSync.Community.DataTypeSerializers.csproj -c $env -o $outFolder /p:ContinuousIntegrationBuild=true,version=$fullversion  
-dotnet pack ..\uSync.BackOffice\uSync.BackOffice.csproj -c $env -o $outFolder /p:ContinuousIntegrationBuild=true,version=$fullVersion 
+    [Parameter()]
+    [string]
+    $suffix, # optional suffix to append to version (for pre-releases)
 
-dotnet pack ..\uSync.AutoTemplates\uSync.AutoTemplates.csproj -c $env -o $outFolder /p:ContinuousIntegrationBuild=true,version=$fullVersion 
+    [Parameter()]
+    [string]
+    $env = 'release', #build environment to use when packing
+
+    [Parameter()]
+    [switch]
+    $push=$false #push to devops nightly feed
+)
+
+$fullVersion = $version;
+
+if (![string]::IsNullOrWhiteSpace($suffix)) {
+   $fullVersion = -join($version, '-', $suffix)
+}
+
+$majorFolder = $version.Substring(0, $version.LastIndexOf('.'))
+
+$outFolder = ".\$majorFolder\$version\$fullVersion"
+if (![string]::IsNullOrWhiteSpace($suffix)) {
+    $suffixFolder = $suffix.substring(0, $suffix.indexOf('.'))
+    $outFolder = ".\$majorFolder\$version\$version-$suffixFolder\$fullVersion"
+}
+
+"----------------------------------"
+Write-Host "Version  :" $fullVersion
+Write-Host "Config   :" $env
+"----------------------------------"; ""
+
+dotnet restore ..
+
+""; "##### Packaging"; "----------------------------------" ; ""
+
+dotnet pack ..\uSync.Core\uSync.Core.csproj --no-restore -c $env -o $outFolder /p:ContinuousIntegrationBuild=true,version=$fullVersion 
+dotnet pack ..\uSync.Community.Contrib\uSync.Community.Contrib.csproj --no-restore -c $env -o $outFolder /p:ContinuousIntegrationBuild=true,version=$fullversion  
+dotnet pack ..\uSync.Community.DataTypeSerializers\uSync.Community.DataTypeSerializers.csproj --no-restore -c $env -o $outFolder /p:ContinuousIntegrationBuild=true,version=$fullversion  
+dotnet pack ..\uSync.BackOffice\uSync.BackOffice.csproj --no-restore -c $env -o $outFolder /p:ContinuousIntegrationBuild=true,version=$fullVersion 
+
+dotnet pack ..\uSync.AutoTemplates\uSync.AutoTemplates.csproj --no-restore -c $env -o $outFolder /p:ContinuousIntegrationBuild=true,version=$fullVersion 
 
 .\nuget pack "..\uSync\uSync.nuspec" -version $fullVersion -OutputDirectory $outFolder
 .\nuget pack "..\uSync.BackOffice.Assets\uSync.BackOffice.StaticAssets.nuspec" -version $fullVersion -OutputDirectory $outFolder
 
-
+""; "##### Copying to LocalGit folder"; "----------------------------------" ; ""
 XCOPY "$outFolder\*.nupkg" "C:\Source\localgit" /Q /Y 
 
 if ($push) {
+    ""; "##### Pushing to our nighly package feed"; "----------------------------------" ; ""
     .\nuget.exe push "$outFolder\*.nupkg" -ApiKey AzureDevOps -src https://pkgs.dev.azure.com/jumoo/Public/_packaging/nightly/nuget/v3/index.json
 }
+
+Write-Host "uSync Packaged : $versionString"
