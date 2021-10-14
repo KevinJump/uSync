7b43a5e802e8fdf332799edea698e9ab881bb210
Kevin Jump <kevin@thejumps.co.uk>
2021-10-14 19:49:42 +0100

    add config extensions to make it easier to move out of appsettings.json.

---
+++ uSync.BackOffice/Configuration/uSyncConfigExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+using System;
+using System.IO;
+
+namespace uSync
+{
+    public static class uSyncConfigExtensions
+    {
+        public static void AdduSyncConfigs(this IConfigurationBuilder configurationBuilder, HostBuilderContext builderContext, string filename)
+        {
+            var env = builderContext.HostingEnvironment;
+            var fileRootName = 
+                Path.Combine(Path.GetDirectoryName(filename), Path.GetFileNameWithoutExtension(filename));
+
+            configurationBuilder.AddJsonFile($"{fileRootName}.json", optional: true, reloadOnChange: true);
+            configurationBuilder.AddJsonFile($"{fileRootName}.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+            configurationBuilder.AddJsonFile($"{fileRootName}.{Environment.MachineName}.json", optional: true, reloadOnChange: true);
+        }
+
+        public static IHostBuilder ConfigureuSyncConfig(this IHostBuilder hostBuilder, string filename = "usync.json")
+        {
+            return hostBuilder.ConfigureAppConfiguration((hostingContext, config) => {
+                config.AdduSyncConfigs(hostingContext, filename);
+            });
+        }
+
+    }
+}
