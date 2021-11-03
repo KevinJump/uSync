717488aa3c2a3a9519dfdd382f0dee85126d57fd
Kevin Jump <kevin@thejumps.co.uk>
2021-11-03 08:46:50 +0000

    min build gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,100 @@
 /// <binding ProjectOpened='default' />
 const { watch, src, dest } = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+const cleanCSS = require('gulp-clean-css');
+const transformManifest = require('./gulp-transformManifest');
 
 const sourceFolders = [
     'uSync.BackOffice.Assets/App_Plugins/'
 ];
 
+const pluginName = 'uSync';
 const destination = 'uSync.Site/App_Plugins/';
+const minDest = 'build-files/App_Plugins/';
 
-function copy(path, baseFolder) {
+// fetch command line arguments
+const arg = (argList => {
+
+    let arg = {}, a, opt, thisOpt, curOpt;
+    for (a = 0; a < argList.length; a++) {
+  
+      thisOpt = argList[a].trim();
+      opt = thisOpt.replace(/^\-+/, '');
+  
+      if (opt === thisOpt) {
+  
+        // argument value
+        if (curOpt) arg[curOpt] = opt;
+        curOpt = null;
+  
+      }
+      else {
+  
+        // argument name
+        curOpt = opt;
+        arg[curOpt] = true;
+  
+      }
+  
+    }
+  
+    return arg;
+  
+  })(process.argv);
+
+  function copy(path, baseFolder) {
     return src(path, { base: baseFolder })
         .pipe(dest(destination));
+}
+
+function minifyJs(path, version) {
+
+    return src(path, { base: path })
+        .pipe(sourcemaps.init())
+            .pipe(concat('usync.' + version + '.min.js'))
+            .pipe(uglify({ mangle: false }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest(minDest + pluginName));
+}
+
+
+function minifyCss(path, version) {
+    return src(path, { base: path })
+        .pipe(cleanCSS({ compatibility: 'ie8'}))
+        .pipe(concat('usync.' + version + '.min.css'))
+        .pipe(dest(minDest + pluginName));
+}
+
+function copyRequired(path, baseFolder, destFolder) {
+    return src([path, '!**/*.manifest', '!**/*.js', '!**/*.css'], { base: baseFolder })
+        .pipe(dest(destFolder));
 }
 
 function time() {
     return '[' + new Date().toISOString().slice(11, -5) + ']';
 }
 
+function updateManifest(manifest, version) {
+
+    return src(manifest)
+        .pipe(transformManifest({
+            folder : pluginName,
+            name: pluginName.toLowerCase(),
+            version: version
+        }))
+        .pipe(dest(minDest + pluginName));
+
+}
+
+
+////////////
 exports.default = function() {
     sourceFolders.forEach(function (sourceFolder) {
 
         let source = sourceFolder + '**/*';
-
+       
         watch(source, { ignoreInitial: false })
             .on('change', function (path, stats) {
                 console.log(time(), path, sourceFolder, 'changed');
@@ -31,4 +105,24 @@ exports.default = function() {
                 copy(path, sourceFolder);
             });
     });
+
 };
+
+exports.minify = function (cb) {
+    
+    var version = arg.release;
+
+    sourceFolders.forEach(function (sourceFolder) {
+        let source = sourceFolder + '**/*';
+        let jsfiles = sourceFolder + '**/*.js';
+        let cssFiles = sourceFolder + '**/*.css';
+
+        minifyJs(jsfiles, version);
+        minifyCss(cssFiles, version);
+        updateManifest(sourceFolder + pluginName + "/package.manifest", version);
+        copyRequired(source, sourceFolder, minDest);
+    });
+
+    cb();
+}
+
---
+++ old-gulpfile.js
@@ -0,0 +1,34 @@
+/// <binding ProjectOpened='default' />
+const { watch, src, dest } = require('gulp');
+
+const sourceFolders = [
+    'uSync.BackOffice.Assets/App_Plugins/'
+];
+
+const destination = 'uSync.Site/App_Plugins/';
+
+function copy(path, baseFolder) {
+    return src(path, { base: baseFolder })
+        .pipe(dest(destination));
+}
+
+function time() {
+    return '[' + new Date().toISOString().slice(11, -5) + ']';
+}
+
+exports.default = function() {
+    sourceFolders.forEach(function (sourceFolder) {
+
+        let source = sourceFolder + '**/*';
+
+        watch(source, { ignoreInitial: false })
+            .on('change', function (path, stats) {
+                console.log(time(), path, sourceFolder, 'changed');
+                copy(path, sourceFolder);
+            })
+            .on('add', function (path, stats) {
+                console.log(time(), path, sourceFolder, 'added');
+                copy(path, sourceFolder);
+            });
+    });
+};
