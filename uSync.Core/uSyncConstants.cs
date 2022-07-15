8bcd07daace81b94d0fc5114623693ad218e28d5
Kevin Jump <kevin@thejumps.co.uk>
2022-07-15 17:18:14 +0100

    #406 - Increment the formatVersion.

--- uSync.Core/uSyncConstants.cs
+++ uSync.Core/uSyncConstants.cs
@@ -26,7 +26,7 @@
         // this is our format 'version' - 
         // it only changes when the format of the .config files change
         // we use it to prompt people to do an uptodate export.
-        public const string FormatVersion = "9.0.4";
+        public const string FormatVersion = "9.0.5";
 
         /// <summary>
         ///  names of the root xml elements that are seralized in/out
