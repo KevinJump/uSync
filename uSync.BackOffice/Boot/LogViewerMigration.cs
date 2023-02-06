ee40d502fa66b08015ccd4a81d5665e4c2a0b3e2
Kevin Jump <kevin@thejumps.co.uk>
2023-02-06 14:05:57 +0000

    add a log search query for uSync log entries.

---
+++ uSync.BackOffice/Boot/LogViewerMigration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+using Umbraco.Cms.Core.Logging.Viewer;
+using Umbraco.Cms.Infrastructure.Migrations;
+
+namespace uSync.BackOffice.Boot;
+internal class LogViewerMigration : MigrationBase
+{
+	private static string uSyncLogQuery = "StartsWith(SourceContext, 'uSync')";
+
+	private readonly ILogViewerConfig _logViewerConfig;
+
+	public LogViewerMigration(
+		ILogViewerConfig config,
+		IMigrationContext context) : base(context)
+	{
+		_logViewerConfig = config;
+	}
+
+	protected override void Migrate()
+	{
+		var existing = _logViewerConfig.GetSavedSearches()
+			.FirstOrDefault(x => x.Query.StartsWith(uSyncLogQuery, StringComparison.OrdinalIgnoreCase));
+
+		if (existing == null)
+		{
+			_logViewerConfig.AddSavedSearch("Find all uSync Log Entries", uSyncLogQuery);
+		}
+	}
+}
