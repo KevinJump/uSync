d88d5310d09934c920a5bd317c93d1630cf0db0d
Kevin Jump <kevin@thejumps.co.uk>
2023-05-16 10:32:01 +0100

    #503 - Additional Published Dependency flag (v8)

--- uSync8.Core/Dependency/DependencyFlags.cs
+++ uSync8.Core/Dependency/DependencyFlags.cs
@@ -16,6 +16,7 @@ namespace uSync8.Core.Dependency
         IncludeConfig = 256, // include config elements such as public access, or domain settings
         AdjacentOnly = 512, // only include direclty adjectent items (don't go right down or up the tree)
         RootSync = 1024, // calculate as part of a sync from the root of a tree. (changes how ancestor syncs are handled)
-        IncludeContents = 2048 // include the content of a file in the xml.
+        IncludeContents = 2048, // include the contents of any file we might be syncing (e.g templates).
+        PublishedDependencies = 4096 // dependencies should be checked against published values.
     }
 }
