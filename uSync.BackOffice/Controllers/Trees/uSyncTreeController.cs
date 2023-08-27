42aa710daeaae550a737fccdb0a5d9f70bf27c0f
Kevin Jump <kevin@thejumps.co.uk>
2023-08-27 21:05:38 +0100

    Add ability to extend child items of the uSync tree node.

--- uSync.BackOffice/Controllers/Trees/uSyncTreeController.cs
+++ uSync.BackOffice/Controllers/Trees/uSyncTreeController.cs
@@ -1,14 +1,21 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Trees;
 using Umbraco.Cms.Web.BackOffice.Trees;
 using Umbraco.Cms.Web.Common.Attributes;
 using Umbraco.Cms.Web.Common.ModelBinders;
+using Umbraco.Extensions;
+
+using uSync.BackOffice.Models;
 
 namespace uSync.BackOffice.Controllers.Trees
 {
@@ -21,13 +28,21 @@ namespace uSync.BackOffice.Controllers.Trees
     [PluginController(uSync.Name)]
     public class uSyncTreeController : TreeController
     {
+        public readonly ITypeFinder _typeFinder;
+
+        public IList<ISyncTreeNode> _treeNodes;
+
         /// <inheritdoc/>
         public uSyncTreeController(
             ILocalizedTextService localizedTextService,
             UmbracoApiControllerTypeCollection umbracoApiControllerTypeCollection,
-            IEventAggregator eventAggregator)
+            IEventAggregator eventAggregator,
+            ITypeFinder typeFinder)
             : base(localizedTextService, umbracoApiControllerTypeCollection, eventAggregator)
-        { }
+        {
+            _typeFinder = typeFinder;
+            _treeNodes = GetTreeNodes();
+        }
 
         /// <inheritdoc/>
         protected override ActionResult<TreeNode> CreateRootNode(FormCollection queryStrings)
@@ -36,7 +51,7 @@ namespace uSync.BackOffice.Controllers.Trees
 
             result.Value.RoutePath = $"{this.SectionAlias}/{uSync.Trees.uSync}/dashboard";
             result.Value.Icon = "icon-infinity";
-            result.Value.HasChildren = false;
+            result.Value.HasChildren = _treeNodes.Count > 0;
             result.Value.MenuUrl = null;
 
             return result.Value;
@@ -51,7 +66,71 @@ namespace uSync.BackOffice.Controllers.Trees
         /// <inheritdoc/>
         protected override ActionResult<TreeNodeCollection> GetTreeNodes(string id, [ModelBinder(typeof(HttpQueryStringModelBinder))] FormCollection queryStrings)
         {
-            return new TreeNodeCollection();
+            if (_treeNodes.Count == 0) return new TreeNodeCollection();
+
+            var collection = new TreeNodeCollection();
+
+            if (id == "-1")
+            {
+                foreach (var node in _treeNodes.OrderBy(x => x.Weight))
+                {
+                    var treeNode = CreateTreeNode(
+                        node.Id,
+                        id,
+                        queryStrings,
+                        node.Title,
+                        node.Icon,
+                        $"{this.SectionAlias}/{node.TreeAlias}/{node.Alias}");
+
+                    var children = node.GetChildNodes(id, queryStrings);
+                    if (children != null)
+                        treeNode.HasChildren = true;
+
+                    collection.Add(treeNode);
+
+                }
+
+                return collection;
+            }
+            else
+            {
+                var treeNode = _treeNodes.FirstOrDefault(x => x.Id == id);
+                if (treeNode != null)
+                {
+                    var children = treeNode.GetChildNodes(id, queryStrings);
+                    if (children != null)
+                    {
+                        foreach(var child in children)
+                        {
+                            collection.Add(CreateTreeNode(
+                                child.Id, 
+                                id, 
+                                queryStrings, 
+                                child.Title,
+                                child.Icon,
+                                $"{this.SectionAlias}/{treeNode.TreeAlias}/{child.Alias}"));
+                        }
+                    }
+                }
+            }
+
+            return collection;
+
+        }
+
+        private IList<ISyncTreeNode> GetTreeNodes()
+        {
+            var treeNodes = new List<ISyncTreeNode>();
+            var nodes = _typeFinder.FindClassesOfType<ISyncTreeNode>();
+            foreach(var node in nodes)
+            {
+                if (Activator.CreateInstance(node) is ISyncTreeNode instance)
+                {
+                    treeNodes.Add(instance);
+                }
+            }
+
+            return treeNodes;
         }
     }
 }
---
+++ uSync.BackOffice/Models/ISyncTreeNode.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Trees;
+using Umbraco.Cms.Web.BackOffice.Trees;
+using Umbraco.Cms.Web.Common.Attributes;
+using Umbraco.Cms.Web.Common.ModelBinders;
+
+namespace uSync.BackOffice.Models
+{
+    /// <summary>
+    ///  add on for uSync that allows you to render a node 
+    ///  under the uSync tree. 
+    /// </summary>
+    public interface ISyncTreeNode
+    {
+        /// <summary>
+        ///  position in the tree, (higher is lower down the tree)
+        /// </summary>
+        public int Weight { get; }
+
+        /// <summary>
+        ///  Id this will be passed to the controller.
+        /// </summary>
+        public string Id { get; }
+
+        /// <summary>
+        ///  alias for the tree
+        /// </summary>
+        public string TreeAlias { get; }
+
+        /// <summary>
+        ///  alias for the node item
+        /// </summary>
+        public string Alias { get; }
+
+        /// <summary>
+        ///  title of the tree item
+        /// </summary>
+        public string Title { get; }
+
+        /// <summary>
+        ///  icon for the tree item. 
+        /// </summary>
+        public string Icon { get; }
+
+
+        /// <summary>
+        ///  method to return any additional child nodes under the parent node
+        /// </summary>
+        public IEnumerable<uSyncTreeNode> GetChildNodes(string id, FormCollection queryStrings);
+
+    }
+
+    public class uSyncTreeNode
+    {
+        public string Id { get; set; }
+        public string Alias { get; set; }
+        public string Title { get; set; }
+        public string Icon { get; set; }
+    }
+}
