6c84ee748963496fab680c93acc1128371a0a99c
henryjump <140804846+henryjump@users.noreply.github.com>
2023-10-06 11:02:46 +0100

    catch and log history exceptions

--- uSync.History/uSyncHistoryNotificationHandler.cs
+++ uSync.History/uSyncHistoryNotificationHandler.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.Hosting;
 using Umbraco.Cms.Core.Security;
@@ -15,12 +16,18 @@ namespace uSync.History
         private readonly IHostingEnvironment _hostingEnvironment;
         private readonly SyncFileService _syncFileService;
         private readonly IBackOfficeSecurityAccessor _backOfficeSecurityAccessor;
+        private readonly ILogger<uSyncHistoryNotificationHandler> _logger;
 
-        public uSyncHistoryNotificationHandler(SyncFileService syncFileService, IBackOfficeSecurityAccessor backOfficeSecurityAccessor, IHostingEnvironment hostingEnvironment)
+        public uSyncHistoryNotificationHandler(
+            SyncFileService syncFileService,
+            IBackOfficeSecurityAccessor backOfficeSecurityAccessor,
+            IHostingEnvironment hostingEnvironment,
+            ILogger<uSyncHistoryNotificationHandler> logger)
         {
             _syncFileService = syncFileService;
             _backOfficeSecurityAccessor = backOfficeSecurityAccessor;
             _hostingEnvironment = hostingEnvironment;
+            _logger = logger;
         }
 
         public void Handle(uSyncImportCompletedNotification notification)
@@ -49,24 +56,31 @@ namespace uSync.History
 
         private void SaveActions(IEnumerable<uSyncAction> actions, string method, int total)
         {
-            var historyInfo = new HistoryInfo
+            try
             {
-                Actions = actions,
-                Date = DateTime.Now,
-                Username = _backOfficeSecurityAccessor?.BackOfficeSecurity?.CurrentUser?.Username ?? "Background Process",
-                Method = method,
-                Total = total,
-                Changes = actions.CountChanges()
-            };
+                var historyInfo = new HistoryInfo
+                {
+                    Actions = actions,
+                    Date = DateTime.Now,
+                    Username = _backOfficeSecurityAccessor?.BackOfficeSecurity?.CurrentUser?.Username ?? "Background Process",
+                    Method = method,
+                    Total = total,
+                    Changes = actions.CountChanges()
+                };
 
-            var historyJson = JsonConvert.SerializeObject(historyInfo, Formatting.Indented);
+                var historyJson = JsonConvert.SerializeObject(historyInfo, Formatting.Indented);
 
-            var rootFolder = _syncFileService.GetAbsPath(_hostingEnvironment.LocalTempPath);
-            var historyFile = Path.Combine(rootFolder, "uSync", "history", DateTime.Now.ToString("dd_MM_yyyy_HH_mm_ss") + ".json");
+                var rootFolder = _syncFileService.GetAbsPath(_hostingEnvironment.LocalTempPath);
+                var historyFile = Path.Combine(rootFolder, "uSync", "history", DateTime.Now.ToString("dd_MM_yyyy_HH_mm_ss") + ".json");
 
-            _syncFileService.CreateFoldersForFile(historyFile);
+                _syncFileService.CreateFoldersForFile(historyFile);
 
-            _syncFileService.SaveFile(historyFile, historyJson);
+                _syncFileService.SaveFile(historyFile, historyJson);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogWarning(ex, "Failed to save history.");
+            }
         }
     }
 }
