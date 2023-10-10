30a8340c692cbea14ab559c4082290b130d36254
Kevin Jump <kevin@thejumps.co.uk>
2023-10-10 10:01:57 +0100

    Comment up the graph handler interface.

--- uSync.BackOffice/SyncHandlers/Interfaces/ISyncGraphableHandler.cs
+++ uSync.BackOffice/SyncHandlers/Interfaces/ISyncGraphableHandler.cs
@@ -4,8 +4,17 @@ using System.Xml.Linq;
 
 namespace uSync.BackOffice.SyncHandlers
 {
+    /// <summary>
+    ///  graphable handler - handler can use a Topological graph
+    ///  to sort items into the most efficient order 
+    /// </summary>
     public interface ISyncGraphableHandler
     {
+        /// <summary>
+        ///  return
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
         public IEnumerable<Guid> GetGraphIds(XElement node);
     }
 }
