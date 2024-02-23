57aabb60dfbc6292c67b79c28e7f327da253bd8c
Kevin Jump <kevin@thejumps.co.uk>
2024-02-23 14:05:59 +0000

    Make usync client library.

---
+++ uSync.Backoffice.Management.Client/assets/README.md
@@ -0,0 +1,10 @@
+## uSync ui library. 
+
+The uSync UI library ,is used by other uSync packages, to 
+show things like changes, and updates ,etc. 
+
+> [!IMPORTANT]
+> Most packages do not need to export thier client library
+as a npm package, unless your client library is also going
+to be used by child packages.
+
---
+++ uSync.Backoffice.Management.Client/assets/vite.dist.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from "vite";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import dts from 'vite-plugin-dts';
+
+/**
+ * library deployment vite.
+ */
+export default defineConfig({
+    build: {
+        lib: {
+            entry: "src/index.ts", // your web component source file
+            name: 'usync',
+            fileName: 'usync',
+            formats: ["es"],
+        },
+        outDir: "./dist",
+        emptyOutDir: true,
+        sourcemap: true,
+        rollupOptions: {
+            external: [/^@umbraco/],
+            onwarn: () => { },
+        },
+    },
+    base: '/usync/',
+    mode: 'production',
+    plugins: [
+        dts(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'src/icons/svg/*.js',
+                    dest: 'icons'
+                }
+            ]
+        })
+    ]
+});
