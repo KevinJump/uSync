2d2f7aa941cd8d3797db6f5a47f476dfc8743ac7
Kevin Jump <kevin@thejumps.co.uk>
2024-05-29 15:02:17 +0100

    RC5 build

--- uSync.Backoffice.Management.Client/usync-assets/src/constants.ts
+++ uSync.Backoffice.Management.Client/usync-assets/src/constants.ts
@@ -4,7 +4,7 @@ const _constants = {
 	icon: 'icon-infinity',
 	menuName: 'Syncronisation',
 	menuAlias: 'usync.menu',
-	version: '14.0.0-rc4',
+	version: '14.0.0-rc5',
 
 	conditions: {
 		legacy: 'usync.legacy.condition',
