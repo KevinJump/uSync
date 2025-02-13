a046af29326d6523f3e451c819b437059571241b
Kevin Jump <kevin@thejumps.co.uk>
2025-02-13 16:54:22 +0000

    regression - regroup the results on the report

--- uSync.Backoffice.Management.Client/usync-assets/src/components/index.ts
+++ uSync.Backoffice.Management.Client/usync-assets/src/components/index.ts
@@ -3,3 +3,4 @@ export * from './usync-action-box.js';
 export * from './usync-progress-box.js';
 export * from './usync-results-view.js';
 export * from './usync-change-view.js';
+export * from './usync-results-group-view.js';
