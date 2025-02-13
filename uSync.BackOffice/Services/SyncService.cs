3fd689b70d54bb1c5149d1365ad9d86e51168e29
Kevin Jump <kevin@thejumps.co.uk>
2025-02-13 16:31:31 +0000

    Firstboot, publish cache refresh when content is imported.

--- uSync.BackOffice/Services/SyncService.cs
+++ uSync.BackOffice/Services/SyncService.cs
@@ -56,6 +56,8 @@ public partial class SyncService : ISyncService
 
     private readonly IAppCache _appCache;
 
+    private readonly DistributedCache _distributedCache;
+
     /// <summary>
     ///  Create a new uSyncService (done via DI)
     /// </summary>
@@ -69,7 +71,8 @@ public partial class SyncService : ISyncService
         AppCaches appCaches,
         ICoreScopeProvider scopeProvider,
         ILoggerFactory loggerFactory,
-        IBackgroundTaskQueue backgroundTaskQueue)
+        IBackgroundTaskQueue backgroundTaskQueue,
+        DistributedCache distributedCache)
     {
         this._logger = logger;
 
@@ -88,7 +91,7 @@ public partial class SyncService : ISyncService
         _loggerFactory = loggerFactory;
 
         _backgroundTaskQueue = backgroundTaskQueue;
-
+        _distributedCache = distributedCache;
     }
 
     /// <inheritdoc/>>
@@ -189,7 +192,15 @@ public partial class SyncService : ISyncService
         handlerOptions ??= new SyncHandlerOptions();
         handlerOptions.Action = HandlerActions.Import;
         var handlers = _handlerFactory.GetValidHandlers(handlerOptions);
-        return await ImportAsync(folders, force, handlers, handlerOptions, callbacks);
+        
+        var changes = await ImportAsync(folders, force, handlers, handlerOptions, callbacks);
+
+        // on first boot, we refresh the snapshot cache if we have imported any content 
+        // just because it seems not to refresh as part of the boot. 
+        if (changes.Any(x => x.Change > ChangeType.NoChange && x.ItemType == "IContent"))
+            _distributedCache.RefreshAllPublishedSnapshot();
+
+        return changes; 
     }
 
     /// <inheritdoc/>>
