8f0d2c009eeeb1c1e6a5dc5dec744e842082f772
Kevin Jump <kevin@thejumps.co.uk>
2025-06-04 13:46:37 +0100

    declare custom event global ?

--- uSync.Backoffice.Management.Client/usync-assets/src/components/events.ts
+++ uSync.Backoffice.Management.Client/usync-assets/src/components/events.ts
@@ -15,3 +15,9 @@ export class uSyncShowDetailEvent extends Event {
 
 	action: USyncActionView;
 }
+
+
+declare global {
+	interface GlobalEventHandlersEventMap {
+		[USYNC_SHOW_DETAIL_EVENT]: uSyncShowDetailEvent;
+}
