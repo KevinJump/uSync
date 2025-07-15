e580a085a89b90048141796cd4a3b0cbe069ca49
Kevin Jump <kevin@thejumps.co.uk>
2025-07-15 11:01:14 +0100

    Update:  Folder collections.

---
+++ uSync.BackOffice/Configuration/ISyncFolder.cs
@@ -0,0 +1,27 @@
+﻿namespace uSync.BackOffice.Configuration;
+
+/// <summary>
+///  folder type to define a folder for the site. 
+/// </summary>
+public interface ISyncFolder
+{
+    /// <summary>
+    ///  path to the folder we want to use. 
+    /// </summary>
+    string Path { get; }
+
+    /// <summary>
+    ///  weight in the list. 
+    /// </summary>
+    int Weight { get; }
+}
+
+
+
+/*
+public class TestSyncFolder: ISyncFolder
+{
+    public string Path => "uSync/TestFolder";
+    public int Weight => 100;
+}
+*/
---
+++ uSync.BackOffice/Configuration/SyncFolderCollection.cs
@@ -0,0 +1,37 @@
+﻿using J2N.Collections.Generic.Extensions;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Umbraco.Cms.Core.Composing;
+
+namespace uSync.BackOffice.Configuration;
+
+/// <summary>
+///  collection of folders loaded from extensions, merged into the 
+///  folders from settings at runtime. 
+/// </summary>
+public class SyncFolderCollection : BuilderCollectionBase<ISyncFolder>
+{
+    public SyncFolderCollection(Func<IEnumerable<ISyncFolder>> items)
+        : base(items)
+    { }
+
+    /// <summary>
+    ///  returns an array of all the folders in the collection.
+    /// </summary>
+    /// <returns></returns>
+    public ISyncFolder[] GetFolders()
+        => this.ToArray();
+}
+
+/// <summary>
+///  collection builder to mange folders. 
+/// </summary>
+public class SyncFolderCollectionBuilder
+    : SetCollectionBuilderBase<SyncFolderCollectionBuilder, SyncFolderCollection, ISyncFolder>
+{
+    protected override SyncFolderCollectionBuilder This => this;
+}
+
