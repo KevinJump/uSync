d99751047a998b94a9f1b50f31776f2331852d97
Kevin Jump <kevin@thejumps.co.uk>
2025-07-15 14:16:43 +0100

    Adds block level recursion to the RTE mappers that contain blocks. (#778)
    * Adds block level recursion to the RTE mappers that contain blocks.
    
    * make sure we don't double call on dependency checks.

--- uSync.Core/Mapping/Mappers/RTEMappers/RTEBlockDataContentMigrator.cs
+++ uSync.Core/Mapping/Mappers/RTEMappers/RTEBlockDataContentMigrator.cs
@@ -1,8 +1,12 @@
-﻿using System.Text.RegularExpressions;
+﻿using Microsoft.Extensions.Logging;
+
+using System.Text.RegularExpressions;
 
 using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models.Blocks;
 using Umbraco.Cms.Core.Services;
 
+using uSync.Core.Dependency;
 using uSync.Core.Extensions;
 
 namespace uSync.Core.Mapping.Mappers.RTEMappers;
@@ -16,36 +20,86 @@ public partial class RTEBlockHelper
 /// <summary>
 ///  updates inline block data-content-udi attributes to data-content-key
 /// </summary>
-public class RTEBlockDataContentMigrator : SyncValueMapperBase, ISyncMapper
+public class RTEBlockDataContentMigrator : SyncBlockMapperBase<RichTextBlockValue>, ISyncMapper
 {
-    public RTEBlockDataContentMigrator(IEntityService entityService)
-        : base(entityService)
+    public RTEBlockDataContentMigrator(
+        IEntityService entityService,
+        IContentTypeService contentTypeService,
+        Lazy<SyncValueMapperCollection> mapperCollection,
+        ILogger<RTEBlockDataContentMigrator> logger)
+        : base(entityService, contentTypeService, mapperCollection, logger)
     { }
 
+    /// <inheritdoc />
     public override string Name => "RTE Block Data Content Mapper";
 
+    /// <inheritdoc />
     public override string[] Editors => [
         "Umbraco.TinyMCE",
         Constants.PropertyEditors.Aliases.RichText,
         $"{Constants.PropertyEditors.Aliases.Grid}.rte"
     ];
 
-    public override Task<string?> GetImportValueAsync(string value, string editorAlias)
+    /// <inheritdoc />
+    public override async Task<string?> GetImportValueAsync(string value, string editorAlias)
     {
         if (value.TryDeserialize<RichTextEditorValue>(out RichTextEditorValue? richTextEditorValue) is false || richTextEditorValue is null)
-            return base.GetImportValueAsync(value, editorAlias);
+            return await base.GetImportValueAsync(value, editorAlias);
 
-        if (RTEBlockHelper.BlockRegex().IsMatch(richTextEditorValue.Markup) is false)
-            return base.GetImportValueAsync(value, editorAlias);
+        richTextEditorValue.Markup = MigrateRTEMarkupBlocks(richTextEditorValue.Markup);
 
-        richTextEditorValue.Markup = RTEBlockHelper.BlockRegex().Replace(
-        richTextEditorValue.Markup,
-        match => UdiParser.TryParse(match.Groups["udi"].Value, out GuidUdi? guidUdi)
-            ? match.Value
-                .Replace(match.Groups["attribute"].Value, "data-content-key")
-                .Replace(match.Groups["udi"].Value, guidUdi.Guid.ToString("D"))
-            : string.Empty);
+        if (richTextEditorValue.Blocks is not null && richTextEditorValue.Blocks?.ContentData.Count > 0)
+        {
+            var blockJson = await base.GetImportValueAsync(richTextEditorValue.Blocks.SerializeJsonString(), editorAlias);
+            if (blockJson is not null)
+            {
+                richTextEditorValue.Blocks = blockJson.DeserializeJson<RichTextBlockValue>();
+            }
+        }
 
-        return Task.FromResult<string?>(richTextEditorValue.SerializeJsonString());
+        return richTextEditorValue.SerializeJsonString();
     }
+
+    /// <inheritdoc />
+    public override async Task<string?> GetExportValueAsync(object value, string editorAlias)
+    {
+        var stringValue = value?.ToString() ?? string.Empty;
+        if (stringValue.TryDeserialize<RichTextEditorValue>(out RichTextEditorValue? richTextEditorValue) is false || richTextEditorValue is null)
+            return stringValue;
+
+        if (richTextEditorValue.Blocks is not null)
+        {
+            var blockJson = await base.GetExportValueAsync(richTextEditorValue.Blocks.SerializeJsonString(), editorAlias);
+            if (blockJson is not null)
+            {
+                richTextEditorValue.Blocks = blockJson.DeserializeJson<RichTextBlockValue>();
+
+            }
+        }
+
+        return richTextEditorValue.SerializeJsonString(true);
+    }
+
+    /// <summary>
+    ///  checks to see if the markup contains any html for block elements, and if so, migrates the data-content-udi attributes to data-content-key.
+    /// </summary>
+    private string MigrateRTEMarkupBlocks(string markup)
+    {
+        if (RTEBlockHelper.BlockRegex().IsMatch(markup) is false)
+            return markup;
+
+        return RTEBlockHelper.BlockRegex().Replace(
+            markup,
+            match => UdiParser.TryParse(match.Groups["udi"].Value, out GuidUdi? guidUdi)
+                ? match.Value
+                    .Replace(match.Groups["attribute"].Value, "data-content-key")
+                    .Replace(match.Groups["udi"].Value, guidUdi.Guid.ToString("D"))
+                : string.Empty);
+    }
+
+    /// <summary>
+    ///  dependency check is done in the core RTE mapper, (which just calls the mapperCollection for blocks.
+    /// </summary>
+    public override Task<IEnumerable<uSyncDependency>> GetDependenciesAsync(object value, string editorAlias, DependencyFlags flags)
+        => Task.FromResult<IEnumerable<uSyncDependency>>([]);
 }
