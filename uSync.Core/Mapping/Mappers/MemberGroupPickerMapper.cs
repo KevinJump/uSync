58b4ad30e15d8007627e4dcddcdb726ca71c3dc3
Kevin Jump <kevin@thejumps.co.uk>
2025-07-23 14:30:31 +0100

    group checker needs to support multiple choice

--- uSync.Core/Mapping/Mappers/MemberGroupPickerMapper.cs
+++ uSync.Core/Mapping/Mappers/MemberGroupPickerMapper.cs
@@ -30,11 +30,24 @@ public class MemberGroupPickerMapper : SyncValueMapperBase, ISyncMapper
     /// </summary>
     public override string GetExportValue(object value, string editorAlias)
     {
-        var attempt = value.TryConvertTo<int>();
-        if (attempt.Success is false) return base.GetExportValue(value, editorAlias);
+        var attempt = value.TryConvertTo<string>();
+        if (attempt.Success is false || attempt.Result is null)
+            return base.GetExportValue(value, editorAlias);
 
-        var group = _memberGroupService.GetById(attempt.Result);
-        return group?.Name ?? base.GetExportValue(value, editorAlias);
+        var values = ConvertToList<int>(attempt.Result.ToDelimitedList());
+
+        var groups = new List<string>();
+
+        foreach (var item in values)
+        {
+            if (item <= 0) continue;
+            // get the group by id
+            var group = _memberGroupService.GetById(item);
+            if (group is not null)
+                groups.Add(group?.Name ?? string.Empty);
+        }
+
+        return string.Join(",", groups);
     }
 
     /// <summary>
@@ -42,10 +55,24 @@ public class MemberGroupPickerMapper : SyncValueMapperBase, ISyncMapper
     /// </summary>
     public override string GetImportValue(string value, string editorAlias)
     {
-        if (string.IsNullOrEmpty(value)) return base.GetImportValue(value, editorAlias);
+        if (string.IsNullOrEmpty(value))
+            return base.GetImportValue(value, editorAlias);
 
-        var group = _memberGroupService.GetByName(value);
-        return group?.Id.ToString() ?? base.GetImportValue(value, editorAlias);
+        var items = value.ToDelimitedList();
+
+        var values = new List<string>();
+
+        foreach (var item in items)
+        {
+            if (string.IsNullOrWhiteSpace(item)) continue;
+
+            // otherwise, we assume its a group name and try to find the group by name
+            var group = _memberGroupService.GetByName(item);
+            if (group is not null)
+                values.Add(group.Id.ToString());
+        }
+
+        return string.Join(",", values);
     }
 
     /// <summary>
@@ -54,23 +81,48 @@ public class MemberGroupPickerMapper : SyncValueMapperBase, ISyncMapper
     public override IEnumerable<uSyncDependency> GetDependencies(object value, string editorAlias, DependencyFlags flags)
     {
         // if we are not including dependencies, then return nothing
-        if (flags.HasFlag(DependencyFlags.IncludeDependencies) is false) 
+        if (flags.HasFlag(DependencyFlags.IncludeDependencies) is false)
             return Enumerable.Empty<uSyncDependency>();
 
         // get the int value and load the group
-        var attempt = value.TryConvertTo<int>();
-        if (attempt.Success is false) return base.GetDependencies(value, editorAlias, flags);
+        var attempt = value.TryConvertTo<string>();
+        if (attempt.Success is false || attempt.Result is null)
+            return base.GetDependencies(value, editorAlias, flags);
 
-        var group = _memberGroupService.GetById(attempt.Result);
-        if (group is null) return base.GetDependencies(value, editorAlias, flags);
+        var values = ConvertToList<int>(attempt.Result.ToDelimitedList());
+        var dependencies = new List<uSyncDependency>();
 
-        return new uSyncDependency
+        foreach (var item in values)
         {
-            Flags = DependencyFlags.None,
-            Name = group.Name,
-            Udi = new GuidUdi(UdiEntityType.MemberGroup, group.Key),
-            Level = 0,
-            Order = DependencyOrders.OrderFromEntityType(UdiEntityType.MemberGroup)
-        }.AsEnumerableOfOne();
+            if (string.IsNullOrWhiteSpace(item.ToString())) continue;
+
+            var group = _memberGroupService.GetById(item);
+            if (group is not null)
+            {
+                dependencies.Add(new uSyncDependency
+                {
+                    Flags = DependencyFlags.None,
+                    Name = group.Name ?? group.Id.ToString(),
+                    Udi = new GuidUdi(UdiEntityType.MemberGroup, group.Key),
+                    Level = 0,
+                    Order = DependencyOrders.OrderFromEntityType(UdiEntityType.MemberGroup)
+                });
+            }
+        }
+
+        return dependencies;
+    }
+
+    private static IEnumerable<T> ConvertToList<T>(IEnumerable<string> items)
+    {
+        foreach (var item in items)
+        {
+            if (string.IsNullOrWhiteSpace(item)) continue;
+            var attempt = item.TryConvertTo<T>();
+            if (attempt.Success && attempt.Result is not null)
+            {
+                yield return attempt.Result;
+            }
+        }
     }
 }
