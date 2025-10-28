00d4226a3ca823945209f9e95fbaa75cd0665fa3
Henry Jump <140804846+henryjump@users.noreply.github.com>
2025-10-28 16:00:30 +0000

    remove wwwroot files

--- uSync.History/wwwroot/App_Plugins/uSync.History/consts-BD6tjpOQ.js
+++
@@ -1,8 +0,0 @@
-const t = {
-  dateStyle: "long",
-  timeStyle: "short"
-};
-export {
-  t as T
-};
-//# sourceMappingURL=consts-BD6tjpOQ.js.map
--- uSync.History/wwwroot/App_Plugins/uSync.History/history.js
+++
@@ -1,6 +0,0 @@
-import { o as i } from "./index-Byf6_J3o.js";
-import "@umbraco-cms/backoffice/auth";
-export {
-  i as onInit
-};
-//# sourceMappingURL=history.js.map
