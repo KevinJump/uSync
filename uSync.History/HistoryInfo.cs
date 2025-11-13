7c0b4b4b5afb519396a4a30c7f507a0650c13592
Kevin Jump <kevin@thejumps.co.uk>
2025-11-13 22:50:13 +0000

    remove newtonsoft from history (#853)

--- uSync.History/HistoryInfo.cs
+++ uSync.History/HistoryInfo.cs
@@ -1,25 +1,20 @@
-﻿using Newtonsoft.Json.Serialization;
-using Newtonsoft.Json;
-using uSync.BackOffice.Models;
+﻿using uSync.BackOffice.Models;
 
-namespace uSync.History
+namespace uSync.History;
+
+public class HistoryInfo
 {
-    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public IEnumerable<uSyncActionView> Actions { get; set; } = [];
 
-    public class HistoryInfo
-    {
-        public IEnumerable<uSyncActionView> Actions { get; set; } = [];
+    public DateTime Date { get; set; }
 
-        public DateTime Date { get; set; }
+    public string Username { get; set; } = string.Empty;
 
-        public string Username { get; set; } = string.Empty;
+    public string Method { get; set; } = string.Empty;
 
-        public string Method { get; set; } = string.Empty;
+    public string FilePath { get; set; } = string.Empty;
 
-        public string FilePath { get; set; } = string.Empty;
+    public int Changes { get; set; }
 
-        public int Changes { get; set; }
-
-        public int Total { get; set; }
-    }
+    public int Total { get; set; }
 }
