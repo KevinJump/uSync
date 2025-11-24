b6369618907c47a8d4bfcdae7fac7f0bf2eda369
Kevin Jump <kevin@thejumps.co.uk>
2025-11-24 12:25:35 +0000

    Update to rc4 with breaking swashbuckle changes. (#864)

--- uSync.Backoffice.Management.Api/Configuration/ConfigSyncApiSwaggerGenOptions.cs
+++ uSync.Backoffice.Management.Api/Configuration/ConfigSyncApiSwaggerGenOptions.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 
 using Swashbuckle.AspNetCore.SwaggerGen;
 
