d1e86b79fc69af8633f6c7f8f092298cb439cd98
Steph Lane <44489591+stephlane@users.noreply.github.com>
2026-01-10 03:35:40 +1300

    Fix grammar in en-US translation (#872)

--- uSync.Backoffice.Management.Client/usync-assets/src/lang/files/en-us.ts
+++ uSync.Backoffice.Management.Client/usync-assets/src/lang/files/en-us.ts
@@ -34,7 +34,7 @@ export default {
 		changeItem: 'Item',
 		changeDiffrence: 'Difference',
 		changeCreate: 'This item does not exist in Umbraco and is being created',
-		noChangesImport: 'No changes where made to this item',
+		noChangesImport: 'No changes were made to this item',
 		noChangesReport: 'No changes detected',
 		noChangesDelete: 'This item has been deleted from Umbraco',
 
