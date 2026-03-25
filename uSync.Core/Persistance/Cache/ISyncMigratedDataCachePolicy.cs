714eb63a006d01d45f70c5c4b54477716476463c
Kevin Jump <kevin@thejumps.co.uk>
2026-03-25 21:56:52 +0000

    Remove the individual cache policy items (we are using full data set cache policies)

--- uSync.Core/Persistance/Cache/ISyncMigratedDataCachePolicy.cs
+++
@@ -1,7 +0,0 @@
-﻿using uSync.Core.Migrations;
-
-namespace uSync.Core.Persistance.Cache;
-
-public interface ISyncMigratedDataCachePolicy 
-    : ISyncDataRepositoryCachePolicy<SyncMigratedData, string>
-{ }
