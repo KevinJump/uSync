780e9153acc5d4a1c0a4d82bb7458f3d61611002
Kevin Jump <kevin@thejumps.co.uk>
2026-04-22 10:38:58 +0100

    update build script so we genete uSync.Extend now too. (#951)

--- dist/build-package.ps1
+++ dist/build-package.ps1
@@ -132,6 +132,7 @@ $projects = "uSync.Core",
     "uSync.Backoffice.Management.Api", 
     "uSync.Backoffice.Management.Client",
     "uSync.History",
+    "uSync.Extend",
     "uSync";
 
 foreach($project in $projects) {
