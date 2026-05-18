03995ee9b14c7272e3bf18d391254b7e1212b35b
Kevin Jump <kevin@thejumps.co.uk>
2026-05-18 17:07:04 +0100

    v18 element trackers

---
+++ uSync.Core/Tracking/Impliment/ElementXmlTracker.cs
@@ -0,0 +1,13 @@
+﻿
+using Umbraco.Cms.Core.Models;
+
+using uSync.Core.Serialization;
+
+namespace uSync.Core.Tracking.Impliment;
+
+public class ElementXmlTracker : ContentBaseTracker<IElement>, ISyncTracker<IElement>
+{
+    public ElementXmlTracker(SyncSerializerCollection serializers)
+        : base(serializers)
+    { }
+}
