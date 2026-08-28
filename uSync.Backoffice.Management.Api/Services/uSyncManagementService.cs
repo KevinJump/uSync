e6bc95dcd906ff3d192b35a2b996366fb607c51d
Kevin Jump <kevin@thejumps.co.uk>
2026-08-28 07:37:13 +0100

    update message / fix obsolete

--- uSync.Backoffice.Management.Api/Services/uSyncManagementService.cs
+++ uSync.Backoffice.Management.Api/Services/uSyncManagementService.cs
@@ -457,7 +457,7 @@ internal class uSyncManagementService : ISyncManagementService
             Status = summaries,
             Actions = actionViews,
             Complete = false,
-            Message = "Processing " + action.ToString()
+            Message = "Processing " + handlerOptions.Handler
         });
 
         return new PerformActionResponse
@@ -619,7 +619,7 @@ internal class uSyncManagementService : ISyncManagementService
     ///  take a zip file as a stream expand it over the current uSync folder. 
     /// </summary>
     public UploadImportResult UnpackStream(Stream stream)
-        => _syncActionService.UnpackImportFromStream(stream);
+        => _syncActionService.UnpackImportFromStreamAsync(stream).GetAwaiter().GetResult();
 
     public async Task<SyncFileVersionCheckResult> GetSyncFileInfo()
         => await _syncVersionFileService.GetSyncFileInfo(_configService.GetWorkingFolder());
